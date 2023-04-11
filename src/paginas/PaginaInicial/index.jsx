44d1749e0d6cf8ac08d20c99c12cf66b86e59b15
Ravi Silva <ravifelipee@gmail.com>
2023-04-10 22:42:22 -0300

    commit inicial

---
+++ src/paginas/PaginaInicial/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Cabecalho from '../../componentes/Cabecalho'
+
+export default function PaginaInicial() {
+    return (
+        <div>
+            <Cabecalho />
+        </div>
+    )
+}
