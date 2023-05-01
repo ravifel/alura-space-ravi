70795bd17f4ebffa4cc24284d849d690cfffa39a
Ravi Silva <ravifelipee@gmail.com>
2023-05-01 16:19:38 -0300

    renderização dinamica das 'Tags'

--- src/componentes/Menu/index.jsx
+++ src/componentes/Menu/index.jsx
@@ -1,9 +1,4 @@
 import React from 'react'
-// import home from '../../assets/icones/home-ativo.png'
-// import maisCurtidas from '../../assets/icones/mais-curtidas-inativo.png'
-// import maisVistas from '../../assets/icones/mais-vistas-inativo.png'
-// import novas from '../../assets/icones/novas-inativo.png'
-// import surpreendaMe from '../../assets/icones/surpreenda-me-inativo.png'
 import styles from './Menu.module.scss'
 import icones from './icones.json'
 
--- src/componentes/Tags/index.jsx
+++ src/componentes/Tags/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import styles from './Tags.module.scss'
+import tags from "./tags.json"
 
 export default function Tags() {
     return (
         <div className={styles.tags}>
             <p className={styles.tags}>Filtre por tags:</p>
             <ul >
-                <li>Estrelas</li>
-                <li>Galáxias</li>
-                <li>Lua</li>
-                <li>Planetas</li>
+                {tags.map((tag) => {
+                    return <li>{tag.titulo}</li>
+                })}
             </ul>
         </div>
     )
