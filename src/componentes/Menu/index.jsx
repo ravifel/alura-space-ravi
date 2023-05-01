c3b818cc03777d87097366ebb16fbf2bcec0494f
Ravi Silva <ravifelipee@gmail.com>
2023-05-01 15:09:43 -0300

    renderização dinamica dos itens do 'Menu'

--- src/componentes/Menu/index.jsx
+++ src/componentes/Menu/index.jsx
@@ -1,35 +1,27 @@
 import React from 'react'
-import home from '../../assets/icones/home-ativo.png'
-import maisCurtidas from '../../assets/icones/mais-curtidas-inativo.png'
-import maisVistas from '../../assets/icones/mais-vistas-inativo.png'
-import novas from '../../assets/icones/novas-inativo.png'
-import surpreendaMe from '../../assets/icones/surpreenda-me-inativo.png'
+// import home from '../../assets/icones/home-ativo.png'
+// import maisCurtidas from '../../assets/icones/mais-curtidas-inativo.png'
+// import maisVistas from '../../assets/icones/mais-vistas-inativo.png'
+// import novas from '../../assets/icones/novas-inativo.png'
+// import surpreendaMe from '../../assets/icones/surpreenda-me-inativo.png'
 import styles from './Menu.module.scss'
+import icones from './icones.json'
 
 export default function Menu() {
     return (
         <nav className={styles.menu}>
             <ul className={styles.menu__lista}>
-                <li className={styles.menu__item}>
-                    <img src={home} alt="" />
-                    <a href="/">Início</a>
-                </li>
-                <li className={styles.menu__item}>
-                    <img src={maisCurtidas} alt="" />
-                    <a href="/">Mais curtidas</a>
-                </li>
-                <li className={styles.menu__item}>
-                    <img src={maisVistas} alt="" />
-                    <a href="/">Mais vistas</a>
-                </li>
-                <li className={styles.menu__item}>
-                    <img src={novas} alt="" />
-                    <a href="/">Novas</a>
-                </li>
-                <li className={styles.menu__item}>
-                    <img src={surpreendaMe} alt="" />
-                    <a href="/">Surpreenda-me</a>
-                </li>
+
+                {icones.map((icone) => {
+                    return (
+                        <div>
+                            <li className={styles.menu__item}>
+                                <img src={icone.imagem} alt="" />
+                                <a href="/">{icone.nome}</a>
+                            </li>
+                        </div>
+                    )
+                })}
             </ul>
         </nav>
     )
