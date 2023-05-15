9bb52d623ef32d87bde172bc1fe630b9a30de0cf
Ravi Silva <ravifelipee@gmail.com>
2023-05-15 17:31:38 -0300

    apresentação das Tags de forma dinamica

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Tags from '../Tags'
 import styles from './Galeria.module.scss'
 import fotos from './fotos.json'
 import Cards from './Cards'
 
 export default function Galeria() {
+
+    const [itens, setItens] = useState(fotos);
+
+    //Função para pegar as TAGS de cada foto.
+    const tags = [...new Set(fotos.map((valor) => valor.tag))]
+    //Foi criada uma constante chamada "TAGS"
+    //Está sendo percorridas todas as fotos e sendo extraido o valor da "Tag"
+    //Como não queremos pegar nomes repetidos de "Tags" foi usada a função "new Set()"
+    //Essas "Tags estão sendo espalhadas dentro de um Array"
+    //Cada posição do Array vai ser preenchida por uma "Tag", que é única.
+
+
     return (
         <section className={styles.galeria} >
+
             <h2>Navegue pela galeria</h2>
-            <Tags />
-            <Cards itens={fotos} styles={styles} />
+
+            {/* As "Tags" vão ser passadas para o componente como uma Props */}
+            <Tags tags={tags} />
+
+            <Cards itens={itens} styles={styles} />
+
         </section >
     )
 }
--- src/componentes/Tags/index.jsx
+++ src/componentes/Tags/index.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 import styles from './Tags.module.scss'
 import tags from "./tags.json"
 
-export default function Tags() {
+export default function Tags({
+    tags
+}) {
     return (
         <div className={styles.tags}>
             <p>Filtre por tags:</p>
             <ul className={styles.tags__lista}>
-                {tags.map((tag) => {
-                    return (
-                        <li key={tag.id}>{tag.titulo}</li>
-                    );
-                })}
-                <li>Todas</li>
+                {tags.map(
+                    (tag) => {
+                        return (
+                            <li key={tag}>{tag}</li>
+                        )
+                    }
+                )}
             </ul>
         </div>
     )
